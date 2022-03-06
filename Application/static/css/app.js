6d25e76cfcebfac6d36258732350330fa5945852
Mrinal Das <dasmrinal216@gmail.com>
2022-03-06 23:38:06 +0530

    could send message and responsive chat-popup

--- Application/static/css/app.js
+++ Application/static/css/app.js
@@ -2,7 +2,7 @@ const popup = document.querySelector('.popup');
 const chatBtn = document.querySelector('.chat-btn');
 const submitbtn = document.querySelector('.submit-btn');
 const chatArea = document.querySelector('.chat-area');
-const inputElm = document.querySelector('input');
+const inputElm = document.querySelector('.chat-input');
 
 //chat button toggeler
 
@@ -14,6 +14,11 @@ chatBtn.addEventListener('click', () => {
 
 submitbtn.addEventListener('click', () => {
     let userInput = inputElm.value;
-    console.log(userInput)
 
+    let temp = `<div class="out-msg">
+    <span class="my-msg">${userInput}</span>
+    </div>`;
+
+    chatArea.insertAdjacentHTML("beforeend", temp);
+    inputElm.value = '';
 })

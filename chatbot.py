df31fbb2d68b6ab554d093a6f2059da653e34972
Mrinal Das <dasmrinal216@gmail.com>
2022-03-12 01:55:26 +0530

    bot train

--- chatbot.py
+++ chatbot.py
@@ -6,22 +6,8 @@ from chatterbot.trainers import ChatterBotCorpusTrainer
 # Creating ChatBot Instance
 chatbot = ChatBot('EDOC')
 
- # Training with Personal Ques & Ans 
-conversation = [
-    "Hello",
-    "Hi there!",
-    "How are you doing?",
-    "I'm doing great.",
-    "That is good to hear",
-    "Thank you.",
-    "You're welcome."
-]
 
-trainer = ListTrainer(chatbot)
-trainer.train(conversation)
-
-# Training with English Corpus Data 
-trainer_corpus = ChatterBotCorpusTrainer(chatbot)
-trainer_corpus.train(
-    'chatterbot.corpus.english'
-) 
+english_bot = ChatBot(
+    "Chatterbot", storage_adapter="chatterbot.storage.SQLStorageAdapter")
+trainer = ChatterBotCorpusTrainer(english_bot)
+trainer.train("data/data.yml")

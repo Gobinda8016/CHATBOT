dae7955e8b5e6a9350ada00fc6cddc333a57f717
Mrinal Das <dasmrinal216@gmail.com>
2022-03-14 13:15:28 +0530

    yml trainer

--- app.py
+++ app.py
@@ -1,2 +1,1 @@
 from Application import app
-
--- chatbot.py
+++ chatbot.py
@@ -1,13 +1,13 @@
 from chatterbot import ChatBot
-from chatterbot.trainers import ListTrainer
 from chatterbot.trainers import ChatterBotCorpusTrainer
 
 
 # Creating ChatBot Instance
 chatbot = ChatBot('EDOC')
 
-
 english_bot = ChatBot(
     "Chatterbot", storage_adapter="chatterbot.storage.SQLStorageAdapter")
 trainer = ChatterBotCorpusTrainer(english_bot)
 trainer.train("data/data.yml")
+trainer.train("data/symptoms.yml")
+trainer.train("data/illness.yml")
